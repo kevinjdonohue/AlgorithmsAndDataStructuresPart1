11f0bc9c77e05281402b16f81f611d2eac459588
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-02-19 17:00:09 -0800

    Added a stack class that internally uses a linked list

---
+++ AlgorithmsAndDataStructuresPart1/Stacks/LinkedListStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AlgorithmsAndDataStructuresPart1.Stacks
+{
+    public class LinkedListStack<T> : IEnumerable<T>
+    {
+        LinkedList<T> _list = new LinkedList<T>();
+
+        public void Push(T item)
+        {
+            _list.AddFirst(item);
+        }
+
+        public T Pop()
+        {
+            if (_list.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            T value = _list.First.Value;
+            _list.RemoveFirst();
+
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (_list.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            return _list.First.Value;
+        }
+
+        public int Count => _list.Count;
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
